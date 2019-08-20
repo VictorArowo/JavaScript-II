8b2d1bc3c8afd061c08e0436972c9053e69f0d1e
Victor Arowo <arowov@gmail.com>
2019-08-20 14:03:18 +0100

    Solved the Challenges

--- assignments/closure.js
+++ assignments/closure.js
@@ -3,6 +3,12 @@
 // Keep it simple! Remember a closure is just a function
 // that manipulates variables defined in the outer scope.
 // The outer scope can be a parent function, or the top level of the script.
+const variableToBeManipulated = "Victor";
+
+const manipulatingClosure= () => {
+  let newVariable = variableToBeManipulated.toUpperCase();
+  console.log(newVariable);
+}
 
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
@@ -16,6 +22,9 @@ const counterMaker = () => {
   //      NOTE: This `counter` function, being nested inside `counterMaker`,
   //      "closes over" the `count` variable. It can "see" it in the parent scope!
   // 3- Return the `counter` function.
+  let count = 0;
+  const counter = () => ++count;
+  return counter;
 };
 // Example usage: const myCounter = counterMaker();
 // myCounter(); // 1
@@ -24,10 +33,20 @@ const counterMaker = () => {
 // ==== Challenge 3: Make `counterMaker` more sophisticated ====
 // It should have a `limit` parameter. Any counters we make with `counterMaker`
 // will refuse to go over the limit, and start back at 1.
+const counterMakerExtra = (limit) => {
+  let count = 0;
+  const counter = () => count < limit ? ++count : count-= limit - 1;
+  return counter;
+}
 
 // ==== Challenge 4: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
+  let counter = 0;
+  return {
+    increment: () =>  ++counter,
+    decrement: () =>  --counter,
+  }
 };
